4a3552757949b54526f3a73f227d303eb3962cd4
faruqhossen30 <faruqhossen30@gmail.com>
2024-08-20 16:03:32 +0600

    review code refactoring

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -16,30 +16,34 @@
                     <div class="col-span-4  group ">
                         <div class=" text-center ">
 
-                            <img src="{{ asset('storage/'.$review->thumbnail) }}" class=" max-w-20 min-h-20 max-h-20  rounded-full" alt="">
+                            <img src="{{ asset('storage/' . $review->thumbnail) }}"
+                                class=" max-w-20 min-h-20 max-h-20  rounded-full" alt="">
 
-                            <div class=" mt-2 mx-5 text-center w-0 h-0 border-[25px]  border-transparent  group-hover:border-b-[#01C778] border-b-[#F8F8F8] border-t-0 "></div>
+                            <div
+                                class=" mt-2 mx-5 text-center w-0 h-0 border-[25px]  border-transparent  group-hover:border-b-[#01C778] border-b-[#F8F8F8] border-t-0 ">
+                            </div>
                         </div>
                         <div class="group bg-[#F8F8F8] p-6 rounded group-hover:bg-[#01C778] group-hover:text-white">
 
-                            <div class="flex group-hover:text-white">
+                            <div class="flex group-hover:text-white filter-white ">
                                 <img src="{{ asset('img/Vectorcoma.png') }}" class="text-white max-w-6" srcset="">
                                 <img src="{{ asset('img/Vectorcoma.png') }}" class="text-white max-w-6" srcset="">
                             </div>
                             <div class="flex text-yellow-400 py-6">
-                                @for ($i =1 ; $i <= $review->rating; $i++)
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-6">
-                                    <path fill-rule="evenodd"
-                                        d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
-                                        clip-rule="evenodd" />
-                                </svg>
+                                @for ($i = 1; $i <= $review->rating; $i++)
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
+                                        class="size-6">
+                                        <path fill-rule="evenodd"
+                                            d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+                                            clip-rule="evenodd" />
+                                    </svg>
                                 @endfor
                             </div>
-                            <p class=" font-normal text-base  leading-[26px]">{!!$review->review!!}</p>
+                            <p class=" font-normal text-base  leading-[26px]">{!! $review->review !!}</p>
 
                             <div class="py-6">
-                                <h4 class="text-[22px] font-bold">{{$review->name}}</h4>
-                                <h6 class=" text-sm">{{$review->designation}}</h6>
+                                <h4 class="text-[22px] font-bold">{{ $review->name }}</h4>
+                                <h6 class=" text-sm">{{ $review->designation }}</h6>
                             </div>
                         </div>
                     </div>
@@ -50,3 +54,12 @@
 
     </div>
 </section>
+
+
+{{-- @push('styles')
+    <style>
+        .filter-white {
+            filter: brightness(0) invert(1);
+        }
+    </style>
+@endpush --}}
